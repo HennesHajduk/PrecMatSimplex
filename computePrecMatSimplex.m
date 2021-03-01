5e42b960613920318dbec2b05e47f25581acf271
hennes <hennes.hajduk@math.tu-dortmund.de>
2021-03-01 17:56:24 +0100

    changes according to proof of MCL-DG paper.

--- computePrecMatSimplex.m
+++ computePrecMatSimplex.m
@@ -10,15 +10,15 @@
 % href TODO
 %
 function CTilde = computePrecMatSimplex(dim, p)
-N = nchoosek(p+dim,p);    % number of local degrees of freedom
+N = nchoosek(p+dim,p);    % amount of local DOF
 CTilde = zeros(N,N,dim);  % initialization
 i2Alpha = mapI2Alpha(p);  % array mapping local indices to multiindices
 volRefSimplex = 1 / factorial(dim); % |\hat K|
 M_L = volRefSimplex / N;  % diagonal entries of lumped mass matrix \hat M_L
 gradBary = [-ones(1,dim); eye(dim)]; % gradients of barycentric coordinates
-for m = 1:dim             % compute \tilde{ \hat C}_m
-  for j = 1:N             % compute (\tilde{ \hat C}_m)_{:,j}
-    alpha = i2Alpha(j,:); % multiindex for j-th local index
+for m = 1:dim             % calc. \tilde{ \hat C}_m
+  for j = 1:N             % calc. (\tilde{ \hat C}_m)_{:,j}
+    alpha = i2Alpha(j,:); % multiindex for jth local index
     for k = 1:dim+1
       for l = 1:dim+1
         beta = alpha;
@@ -27,12 +27,12 @@ for m = 1:dim             % compute \tilde{ \hat C}_m
         if (beta(l) > p || beta(k) < 0)
           continue;
         end
-        i = getIfromAlpha(p,beta); % local index corresponding to beta
-        CTilde(i,j,m) = CTilde(i,j,m) + gradBary(k,m) ... % cf. {MCL-DG}
-                                        * (alpha(l) - (l==k) + 1); % (B.1)
+        i = getIfromAlpha(p,beta); % local index for beta
+        CTilde(i,j,m) = CTilde(i,j,m) ...   % cf. % (B.1)
+            + gradBary(k,m)  * (alpha(l) - (l==k) + 1);
       end
     end
   end
 end
-CTilde = M_L * CTilde; % multiplication with lumped mass matrix
+CTilde = M_L * CTilde; % multiply with lumped mass matrix
 end
--- mapI2Alpha.m
+++ mapI2Alpha.m
@@ -3,19 +3,19 @@
 % The degrees of freedom are ordered in lexicographical manner.
 %
 function i2alpha = mapI2Alpha(p)
-dim = 2;                      % spacial dimension
-N = nchoosek(p+dim,p);        % number of local degrees of freedom
+dim = 2;                      % spatial dimension
+N = nchoosek(p+dim,p);        % amount of local DOF
 i2alpha = zeros(N,dim+1);     % initialization
-a = [p 0 0];                  % first multiindex
+a = [p 0 0];                  % first DOF
 ctr = 1;                      % counter variable
-for j = 0:p                   % step through DOF in y-direction
-  for i = 0:p-j               % step through DOF in x-direction
+for j = 0:p                   % step through DOF in y-dir.
+  for i = 0:p-j               % step through DOF in x-dir.
     i2alpha(ctr,:) = a;       % fill map entry
     ctr = ctr+1;              % update counter
-    a(1:2) = a(1:2) + [-1 1]; % go to next multiindex in x-direction
+    a(1:2) = a(1:2) + [-1 1]; % go to next DOF in x-dir.
   end
-  a(1) = p-1-j;               % go to next multiindex in y-direction
-  a(2) = 0;                   % go to first DOF in x-direction
-  a(3) = a(3)+1;              % go to next DOF in y-direction
+  a(1) = p-1-j;               % go to next DOF in y-dir.
+  a(2) = 0;                   % go to first DOF in x-dir.
+  a(3) = a(3)+1;              % go to next DOF in y-dir.
 end
 end
